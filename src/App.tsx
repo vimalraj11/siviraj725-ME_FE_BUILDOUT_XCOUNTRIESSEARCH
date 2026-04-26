40ed917a6d0f6ce74ade7e346db85aa7640cd5f9
vimalraj11 <84240545+vimalraj11@users.noreply.github.com>
2026-04-26 15:53:35 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,28 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch(
-      "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
-    )
-      .then((res) => res.json())
-      .then((data: Country[]) => setCountries(data))
-      .catch(() => alert("failed to fetch data"));
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+        );
+        const data: Country[] = await res.json();
+        setCountries(data);
+      } catch (error) {
+        console.error(error); // important for test
+      }
+    };
+
+    fetchData();
   }, []);
 
-  const filtered = countries.filter((country) =>
+  const filteredCountries = countries.filter((country) =>
     country.common.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
     <div className="app">
+      {/* SEARCH INPUT */}
       <input
         type="text"
         placeholder="Search for countries..."
@@ -32,9 +40,10 @@ function App() {
         onChange={(e) => setSearch(e.target.value)}
       />
 
-      <div className="countries-container">
-        {filtered.map((country, index) => (
-          <div className="country-card" key={index}>
+      {/* COUNTRIES */}
+      <div className="countriesContainer">
+        {filteredCountries.map((country, index) => (
+          <div className="countryCard" key={index}>
             <img src={country.png} alt={country.common} />
             <p>{country.common}</p>
           </div>
